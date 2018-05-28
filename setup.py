c04f0d2c405f07fec13bc8450a17f35ba4f4fb5f
jlstevens <jlstevens@ed.ac.uk>
2018-05-28 16:38:18 +0100

    Removed embed_version code in favor of building wheels

--- setup.py
+++ setup.py
@@ -7,32 +7,6 @@ except ImportError:
     from distutils.core import setup
 
 
-
-def embed_version(basepath, ref='v0.2.2'):
-    """
-    Autover is purely a build time dependency in all cases (conda and
-    pip) except for when you use pip's remote git support [git+url] as
-    1) you need a dynamically changing version and 2) the environment
-    starts off clean with zero dependencies installed.
-    This function acts as a fallback to make Version available until
-    PEP518 is commonly supported by pip to express build dependencies.
-    """
-    import io, zipfile, importlib
-    try:    from urllib.request import urlopen
-    except: from urllib import urlopen
-    try:
-        url = 'https://github.com/ioam/autover/archive/{ref}.zip'
-        response = urlopen(url.format(ref=ref))
-        zf = zipfile.ZipFile(io.BytesIO(response.read()))
-        ref = ref[1:] if ref.startswith('v') else ref
-        embed_version = zf.read('autover-{ref}/autover/version.py'.format(ref=ref))
-        with open(os.path.join(basepath, 'version.py'), 'wb') as f:
-            f.write(embed_version)
-        return importlib.import_module("version")
-    except:
-        return None
-
-
 def get_setup_version(reponame):
     """
     Helper to get the current version from either git describe or the
@@ -44,7 +18,7 @@ def get_setup_version(reponame):
     try:
         from param import version
     except:
-        version = embed_version(basepath)
+        version = None
     if version is not None:
         return version.Version.setup_version(basepath, reponame, archive_commit="$Format:%h$")
     else:
