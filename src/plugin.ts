40e2ffb2af24362515a755d6f1673fed683ef25e
adam <athornton@gmail.com>
2019-04-23 13:02:13 -0700

    Update dependencies

--- src/plugin.ts
+++ src/plugin.ts
@@ -8,7 +8,7 @@ import {
 } from '@jupyterlab/notebook'
 
 import {
-  JupyterLabPlugin,
+  JupyterFrontEndPlugin,
   JupyterLab
 } from '@jupyterlab/application'
 
@@ -30,41 +30,41 @@ import {
 
 
 export
-type INBWidgetExtension = DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>;
+  type INBWidgetExtension = DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>;
 
 
 export
-class NBWidgetExtension implements INBWidgetExtension {
+  class NBWidgetExtension implements INBWidgetExtension {
   createNew(nb: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
     let manager = new ContextManager(context);
 
     nb.rendermime.addFactory({
-        safe: false,
-        mimeTypes: [HV_LOAD_MIME_TYPE],
-        createRenderer: (options) => new HVJSLoad(options)
+      safe: false,
+      mimeTypes: [HV_LOAD_MIME_TYPE],
+      createRenderer: (options) => new HVJSLoad(options)
     }, -1);
 
     nb.rendermime.addFactory({
-        safe: false,
-        mimeTypes: [HV_EXEC_MIME_TYPE],
-        createRenderer: (options) => new HVJSExec(options, manager)
+      safe: false,
+      mimeTypes: [HV_EXEC_MIME_TYPE],
+      createRenderer: (options) => new HVJSExec(options, manager)
     }, -1);
 
     return new DisposableDelegate(() => {
-        if (nb.rendermime) {
-            nb.rendermime.removeMimeType(HV_EXEC_MIME_TYPE);
-        }
-        manager.dispose();
+      if (nb.rendermime) {
+        nb.rendermime.removeMimeType(HV_EXEC_MIME_TYPE);
+      }
+      manager.dispose();
     });
   }
 }
 
 export
-  const extension: JupyterLabPlugin<void> = {
+  const extension: JupyterFrontEndPlugin<void> = {
     id: 'jupyterlab_holoviews',
     autoStart: true,
     activate: (app: JupyterLab) => {
-        // this adds the HoloViews widget extension onto Notebooks specifically
-        app.docRegistry.addWidgetExtension('Notebook', new NBWidgetExtension());
+      // this adds the HoloViews widget extension onto Notebooks specifically
+      app.docRegistry.addWidgetExtension('Notebook', new NBWidgetExtension());
     }
-}
+  }
