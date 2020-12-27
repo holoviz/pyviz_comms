a55df787acabbdf6318e90b4e122c315ebdfba6b
Philipp Rudiger <prudiger@anaconda.com>
2020-12-27 19:16:06 +0100

    Fix conda build

--- conda.recipe/bld.bat
+++
@@ -1,4 +0,0 @@
-python setup.py --quiet install --single-version-externally-managed --record record.txt
-mkdir "%RECIPE_DIR%\share\jupyter\labextensions\@pyviz\jupyter_pyviz"
-xcopy /S "%SRC_DIR%\pyviz_comms\labextension\*" "%PREFIX%\share\jupyter\labextensions\@pyviz\jupyter_pyviz\"
-xcopy "%SRC_DIR%\pyviz_comms\labextension\install.json" "%PREFIX%\share\jupyter\labextensions\@pyviz\jupyter_pyviz\install.json"
